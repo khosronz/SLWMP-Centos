3eed91744ff5b9a1643589aad36af3462554b49d
Tim Scharner <tim@scharner.me>
2018-04-22 21:08:00 +0200

    Cleaning code

--- installer.sh
+++ installer.sh
@@ -37,7 +37,7 @@ choice () {
 initialize_os() {
   if [ $DISTRO = "debian" ]; then
     # MariaDB
-    apt-get -qq install expect curl software-properties-common dirmngr -y < /dev/null > /dev/null
+    apt-get -qq install curl wget software-properties-common dirmngr -y < /dev/null > /dev/null
     DEBIAN_FRONTEND=noninteractive apt-get -qq update < /dev/null > /dev/null
 
     DEBIAN_FRONTEND=noninteractive apt-key adv --recv-keys -qq --keyserver keyserver.ubuntu.com 0xF1656F24C74CD1D8 < /dev/null > /dev/null
@@ -60,8 +60,8 @@ initialize_os() {
   fi
 
   if [ $DISTRO = "centos" ]; then
-
-    #maridb
+    yum -q update -y
+    yum -q install curl wget -y
     cat >/etc/yum.repos.d/MariaDB.repo <<EOL
 [mariadb]
 name = MariaDB
@@ -69,12 +69,8 @@ baseurl = http://yum.mariadb.org/10.2/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 EOL
-    rpm --import https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
-    yum update -y
-
-    #nginx
-    cd /tmp && wget --quiet http://nginx.org/keys/nginx_signing.key
-    rpm --import nginx_signing.key
+    rpm --import https://yum.mariadb.org/RPM-GPG-KEY-MariaDB > /dev/null
+    rpm --import http://nginx.org/keys/nginx_signing.key> /dev/null
 cat >/etc/yum.repos.d/nginx.repo <<EOL
 [nginx]
 name=nginx repo
@@ -82,31 +78,26 @@ baseurl=http://nginx.org/packages/mainline/centos/7/\$basearch/
 gpgcheck=0
 enabled=1
 EOL
-
-    #php
-    # Using Remis Repo https://rpms.remirepo.net/
-    yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y > /dev/null
-    yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y > /dev/null
-    yum update -y && yum install yum-utils -y > /dev/null
-
+    yum -q update -y
+    yum -q install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
+    yum -q install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+    yum -q update -y && yum -q install yum-utils -y > /dev/null
   fi
 }
 
 install_nginx() {
-    if servicesCheck "nginx"; then
-                if [ $DISTRO = "debian" ]; then
-                  DEBIAN_FRONTEND=noninteractive apt-get -qq install nginx -y < /dev/null > /dev/null
-              	fi
-                if [ $DISTRO = "centos" ]; then
-                  # Addd sources for centos from nginx website.
-                  yum update -y && yum install nginx -y > /dev/null
-                  rm -f /tmp/nginx_signing.key
-                fi
-                return 0
-    else
-      return 1
+  if servicesCheck "nginx"; then
+    if [ $DISTRO = "debian" ]; then
+      DEBIAN_FRONTEND=noninteractive apt-get -qq install nginx -y < /dev/null > /dev/null
     fi
-
+    if [ $DISTRO = "centos" ]; then
+      yum update -y && yum install nginx -y > /dev/null
+      rm -f /tmp/nginx_signing.key
+    fi
+    return 0
+  else
+    return 1
+  fi
 }
 
 install_mariadb(){
@@ -119,11 +110,10 @@ install_mariadb(){
 
     fi
     if [ $DISTRO = "centos" ]; then
-      yum install MariaDB-server MariaDB-client -y > /dev/null
+      yum -q install MariaDB-server MariaDB-client -y > /dev/null
 
       systemctl -q enable mariadb
       systemctl -q start mariadb
-
 
       return 0
     fi
@@ -134,62 +124,61 @@ install_mariadb(){
 
 install_phpfpm() {
 if servicesCheck "php-fpm"; then
-                if [ $DISTRO = "debian" ]; then
-                  for opt in "${!php_opts[@]}"
-                  do
-                    if [[ ${php_opts[opt]} ]];then
-                          if (($opt=="1")); then
-                            DEBIAN_FRONTEND=noninteractive apt-get -qq install php7.0-fpm php7.0-mysql php7.0-gd php7.0-cli php7.0-curl php7.0-mbstring php7.0-posix php7.0-mcrypt php7.0-xml php7.0-xmlrpc php7.0-intl php7.0-mcrypt php7.0-imagick php7.0-xml php7.0-zip -y < /dev/null > /dev/null
-                            systemctl -q start php7.0-fpm
-                            systemctl -q enable php7.0-fpm
-                            printf "  - PHP 7.0 installed [X]"
-                          fi
-                          if (($opt=="2")); then
-                            DEBIAN_FRONTEND=noninteractive apt-get -qq install php7.1-fpm php7.1-mysql php7.1-gd php7.1-cli php7.1-curl php7.1-mbstring php7.1-posix php7.1-mcrypt php7.1-xml php7.1-xmlrpc php7.1-intl php7.1-mcrypt php7.1-imagick php7.1-xml php7.1-zip -y < /dev/null > /dev/null
-                            systemctl -q start php7.1-fpm
-                            systemctl -q enable php7.1-fpm
-                            printf "  - PHP 7.1 installed [X]"
-                          fi
-                          if (($opt=="3")); then
-                            DEBIAN_FRONTEND=noninteractive apt-get -qq install php7.2-fpm php7.2-mysql php7.2-gd php7.2-cli php7.2-curl php7.2-mbstring php7.2-posix php7.2-xml php7.2-xmlrpc php7.2-intl php7.2-imagick php7.2-xml php7.2-zip -y < /dev/null > /dev/null
-                            systemctl -q start php7.2-fpm
-                            systemctl -q enable php7.2-fpm
-                            printf "  - PHP 7.2 installed [X]"
-                          fi
-                    fi
-                  done
-                  return 0
-                fi
-                if [ $DISTRO = "centos" ]; then
-                  for opt in "${!php_opts[@]}"
-                  do
-                    if [[ ${php_opts[opt]} ]];then
-                          if (($opt=="1")); then
-                            yum install php70-php-fpm php70-php-mysql php70-php-gd php70-php-cli php70-php-curl php70-php-mbstring php70-php-posix php70-php-mcrypt php70-php-xml php70-php-xmlrpc php70-php-intl php70-php-mcrypt php70-php-imagick php70-php-xml php70-php-zip -y > /dev/null
-                            systemctl -q start php70-php-fpm
-                            systemctl -q enable php70-php-fpm
-                            printf "  - PHP 7.0 installed [X]"
-                          fi
-                          if (($opt=="2")); then
-                            apt install php7.1-fpm php7.1-mysql php7.1-gd php7.1-cli php7.1-curl php7.1-mbstring php7.1-posix php7.1-mcrypt php7.1-xml php7.1-xmlrpc php7.1-intl php7.1-mcrypt php7.1-imagick php7.1-xml php7.1-zip -y > /dev/null
-                            systemctl -q start php7.1-fpm
-                            systemctl -q enable php7.1-fpm
-                            printf "  - PHP 7.1 installed [X]"
-                          fi
-                          if (($opt=="3")); then
-                            yum install php72-php-fpm php72-php-mysql php72-php-gd php72-php-cli php72-php-curl php72-php-mbstring php72-php-posix php72-php-xml php72-php-xmlrpc php72-php-intl php72-php-imagick php72-php-xml php72-php-zip -y > /dev/null
-                            systemctl -q start php72-php-fpm
-                            systemctl -q enable php72-php-fpm
-                            printf "  - PHP 7.2 installed [X]"
-                          fi
-                    fi
-                  done
-
-                  return 0
-                fi
-else
-  return 1
-fi
+  if [ $DISTRO = "debian" ]; then
+    for opt in "${!php_opts[@]}"
+      do
+        if [[ ${php_opts[opt]} ]];then
+          if (($opt=="1")); then
+            DEBIAN_FRONTEND=noninteractive apt-get -qq install php7.0-fpm php7.0-mysql php7.0-gd php7.0-cli php7.0-curl php7.0-mbstring php7.0-posix php7.0-mcrypt php7.0-xml php7.0-xmlrpc php7.0-intl php7.0-mcrypt php7.0-imagick php7.0-xml php7.0-zip -y < /dev/null > /dev/null
+            systemctl -q start php7.0-fpm
+            systemctl -q enable php7.0-fpm
+            printf "  - PHP 7.0 installed [X]"
+          fi
+          if (($opt=="2")); then
+            DEBIAN_FRONTEND=noninteractive apt-get -qq install php7.1-fpm php7.1-mysql php7.1-gd php7.1-cli php7.1-curl php7.1-mbstring php7.1-posix php7.1-mcrypt php7.1-xml php7.1-xmlrpc php7.1-intl php7.1-mcrypt php7.1-imagick php7.1-xml php7.1-zip -y < /dev/null > /dev/null
+            systemctl -q start php7.1-fpm
+            systemctl -q enable php7.1-fpm
+            printf "  - PHP 7.1 installed [X]"
+          fi
+          if (($opt=="3")); then
+            DEBIAN_FRONTEND=noninteractive apt-get -qq install php7.2-fpm php7.2-mysql php7.2-gd php7.2-cli php7.2-curl php7.2-mbstring php7.2-posix php7.2-xml php7.2-xmlrpc php7.2-intl php7.2-imagick php7.2-xml php7.2-zip -y < /dev/null > /dev/null
+            systemctl -q start php7.2-fpm
+            systemctl -q enable php7.2-fpm
+            printf "  - PHP 7.2 installed [X]"
+          fi
+        fi
+      done
+      return 0
+    fi
+    if [ $DISTRO = "centos" ]; then
+      for opt in "${!php_opts[@]}"
+      do
+        if [[ ${php_opts[opt]} ]];then
+          if (($opt=="1")); then
+            yum -q install php70-php-fpm php70-php-mysql php70-php-gd php70-php-cli php70-php-curl php70-php-mbstring php70-php-posix php70-php-mcrypt php70-php-xml php70-php-xmlrpc php70-php-intl php70-php-mcrypt php70-php-imagick php70-php-xml php70-php-zip -y > /dev/null
+            systemctl -q start php70-php-fpm
+            systemctl -q enable php70-php-fpm
+            printf "  - PHP 7.0 installed [X]"
+          fi
+          if (($opt=="2")); then
+            yum -q install php71-php-fpm php71-php-mysql php71-php-gd php71-php-cli php71-php-curl php71-php-mbstring php71-php-posix php71-php-mcrypt php71-php-xml php71-php-xmlrpc php71-php-intl php71-php-mcrypt php71-php-imagick php71-php-xml php71-php-zip -y
+            systemctl -q start php7.1-fpm
+            systemctl -q enable php7.1-fpm
+            printf "  - PHP 7.1 installed [X]"
+          fi
+          if (($opt=="3")); then
+            yum -q install php72-php-fpm php72-php-mysql php72-php-gd php72-php-cli php72-php-curl php72-php-mbstring php72-php-posix php72-php-xml php72-php-xmlrpc php72-php-intl php72-php-imagick php72-php-xml php72-php-zip -y > /dev/null
+            systemctl -q start php72-php-fpm
+            systemctl -q enable php72-php-fpm
+            printf "  - PHP 7.2 installed [X]"
+          fi
+        fi
+      done
+      return 0
+    fi
+  else
+    return 1
+  fi
 }
 
 install_letsencrypt() {
@@ -267,47 +256,47 @@ then
   PS3='Please select your PHP versions: '
   while :
   do
-      clear
-      options=("PHP 7.0 ${php_opts[1]}" "PHP 7.1 ${php_opts[2]}" "PHP 7.2 ${php_opts[3]}" "Done")
-      select opt in "${options[@]}"
-      do
-          case $opt in
-              "PHP 7.0 ${php_opts[1]}")
-                  choice 1
-                  break
-                  ;;
-              "PHP 7.1 ${php_opts[2]}")
-                  choice 2
-                  break
-                  ;;
-              "PHP 7.2 ${php_opts[3]}")
-                  choice 3
-                  break
-                  ;;
-              "Done")
-                  break 2
-                  ;;
-              *) printf '%s\n' 'invalid option';;
-          esac
-      done
-    done
     clear
-    printf "######################################\nInstallation of SLEMP will start now\n######################################\n"
-    printf "Installing nginx . . . "
-    if install_nginx $1; then echo "[X]"; else echo "Failed..."; fi
-    printf "\nConfiguring nginx . . ."
-    if initialize_nginx $1; then echo "[X]"; else echo "Failed..."; fi
-    printf "\nInstalling MariaDB . . . "
-    if install_mariadb $1; then echo "[X]"; else echo "Failed..."; fi
-    printf "\nInstalling PHP . . . "
-    if install_phpfpm $1; then echo ""; else echo "Failed..."; fi
-    #install_letsencrypt
+    options=("PHP 7.0 ${php_opts[1]}" "PHP 7.1 ${php_opts[2]}" "PHP 7.2 ${php_opts[3]}" "Done")
+    select opt in "${options[@]}"
+    do
+      case $opt in
+        "PHP 7.0 ${php_opts[1]}")
+          choice 1
+          break
+          ;;
+        "PHP 7.1 ${php_opts[2]}")
+          choice 2
+          break
+          ;;
+        "PHP 7.2 ${php_opts[3]}")
+          choice 3
+          break
+          ;;
+        "Done")
+          break 2
+          ;;
+        *) printf '%s\n' 'invalid option';;
+      esac
+    done
+  done
+  clear
+  printf "######################################\nInstallation of SLEMP will start now\n######################################\n"
+  printf "Installing nginx . . . "
+  if install_nginx $1; then echo "[X]"; else echo "Failed..."; fi
+  printf "\nConfiguring nginx . . ."
+  if initialize_nginx $1; then echo "[X]"; else echo "Failed..."; fi
+  printf "\nInstalling MariaDB . . . "
+  if install_mariadb $1; then echo "[X]"; else echo "Failed..."; fi
+  printf "\nInstalling PHP . . . "
+  if install_phpfpm $1; then echo ""; else echo "Failed..."; fi
+  #install_letsencrypt
 
-    if [ $DISTRO = "centos" ]; then
-      printf "Configure CentOS . . ."
-      configure_centos
-    fi
-    [ $? -ne "0" ] && exit 1
+  if [ $DISTRO = "centos" ]; then
+    printf "Configure CentOS . . ."
+    configure_centos
+  fi
+  [ $? -ne "0" ] && exit 1
 fi
 
 echo <<EOF "
