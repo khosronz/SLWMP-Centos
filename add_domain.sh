13c216ec0c8e56cd00a91b62053c8f3ec427cc84
Tim <mail@timscha.io>
2018-06-01 11:21:26 +0200

    Complete Workflow

--- add_domain.sh
+++ add_domain.sh
@@ -25,24 +25,24 @@ fi
 create_skeleton_dirs() {
 
   if [ ! id -u $HOST_LOCATION_USER >/dev/null 2>&1 ]; then
-	useradd $HOST_LOCATION_USER -d /var/www/$USER_MAINDOMAIN
-	usermod -aG $HOST_LOCATION_USER nginx
+	   useradd $HOST_LOCATION_USER -d /var/www/$USER_MAINDOMAIN
+	   usermod -aG $HOST_LOCATION_USER nginx
   fi
-  
+
   if [ ! -d /var/www/$USER_MAINDOMAIN ]; then
-	mkdir -p /var/www/$USER_MAINDOMAIN/htdocs
-	mkdir /var/www/$USER_MAINDOMAIN/logs
-	mkdir /var/www/$USER_MAINDOMAIN/tmp
+	   mkdir -p /var/www/$USER_MAINDOMAIN/htdocs
+	   mkdir /var/www/$USER_MAINDOMAIN/logs
+	   mkdir /var/www/$USER_MAINDOMAIN/tmp
   fi
 
   if [ $USER_DOMAIN_TYP = "1" ]; then
 	if [ ! -d /var/www/$USER_MAINDOMAIN/$USER_SUBDOMAIN ]; then
-		mkdir -p /var/www/$USER_MAINDOMAIN/$USER_SUBDOMAIN/htdocs
+    mkdir -p /var/www/$USER_MAINDOMAIN/$USER_SUBDOMAIN/htdocs
 		mkdir /var/www/$USER_MAINDOMAIN/$USER_SUBDOMAIN/logs
 		mkdir /var/www/$USER_MAINDOMAIN/$USER_SUBDOMAIN/tmp
 	fi
   fi
-  
+
   chown -R $HOST_LOCATION_USER: /var/www/$USER_MAINDOMAIN
   chmod 755 /var/www/$USER_MAINDOMAIN
   chown -R $HOST_LOCATION_USER: /var/www/$USER_MAINDOMAIN/$USER_SUBDOMAIN
@@ -57,38 +57,38 @@ configure_fpm_pool(){
     if [ $USER_PHP_VERSION = "php72" ]; then
       if [ $USER_DOMAIN_TYP = "0" ]; then
         cp templates/phpfpmpool.template /etc/php/7.2/fpm/pool.d/$USER_MAINDOMAIN.conf
-		
-		sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/php/7.2/fpm/pool.d/$USER_MAINDOMAIN.conf
+
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/php/7.2/fpm/pool.d/$USER_MAINDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/php/7.2/fpm/pool.d/$USER_MAINDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_MAINDOMAIN_ROOT_LOCATION'|g' /etc/php/7.2/fpm/pool.d/$USER_MAINDOMAIN.conf
-		sed -i s/PHP-SOCKET/php72-fpm-$USER_DOMAIN_HYPHEN/g /etc/php/7.2/fpm/pool.d/$USER_MAINDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php72-fpm-$USER_DOMAIN_HYPHEN/g /etc/php/7.2/fpm/pool.d/$USER_MAINDOMAIN.conf
       fi
       if [ $USER_DOMAIN_TYP = "1" ]; then
         cp templates/phpfpmpool.template /etc/php/7.2/fpm/pool.d/$USER_SUBDOMAIN.conf
 
-		sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.2/fpm/pool.d/$USER_SUBDOMAIN.conf
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.2/fpm/pool.d/$USER_SUBDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/php/7.2/fpm/pool.d/$USER_SUBDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_SUBDOMAIN_ROOT_LOCATION'|g' /etc/php/7.2/fpm/pool.d/$USER_SUBDOMAIN.conf
-		sed -i s/PHP-SOCKET/php72-fpm-$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.2/fpm/pool.d/$USER_MAINDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php72-fpm-$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.2/fpm/pool.d/$USER_MAINDOMAIN.conf
       fi
       systemctl reload php7.2-fpm
     fi
     if [ $USER_PHP_VERSION = "php71" ]; then
       if [ $USER_DOMAIN_TYP = "0" ]; then
         cp templates/phpfpmpool.template /etc/php/7.1/fpm/pool.d/$USER_MAINDOMAIN.conf
-		
-		sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/php/7.1/fpm/pool.d/$USER_MAINDOMAIN.conf
+
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/php/7.1/fpm/pool.d/$USER_MAINDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/php/7.1/fpm/pool.d/$USER_MAINDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_MAINDOMAIN_ROOT_LOCATION'|g' /etc/php/7.1/fpm/pool.d/$USER_MAINDOMAIN.conf
-		sed -i s/PHP-SOCKET/php71-fpm-$USER_DOMAIN_HYPHEN/g /etc/php/7.1/fpm/pool.d/$USER_MAINDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php71-fpm-$USER_DOMAIN_HYPHEN/g /etc/php/7.1/fpm/pool.d/$USER_MAINDOMAIN.conf
       fi
       if [ $USER_DOMAIN_TYP = "1" ]; then
         cp templates/phpfpmpool.template /etc/php/7.1/fpm/pool.d/$USER_SUBDOMAIN.conf
 
-		sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.1/fpm/pool.d/$USER_SUBDOMAIN.conf
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.1/fpm/pool.d/$USER_SUBDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/php/7.1/fpm/pool.d/$USER_SUBDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_SUBDOMAIN_ROOT_LOCATION'|g' /etc/php/7.1/fpm/pool.d/$USER_SUBDOMAIN.conf
-		sed -i s/PHP-SOCKET/php71-fpm-$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.1/fpm/pool.d/$USER_SUBDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php71-fpm-$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.1/fpm/pool.d/$USER_SUBDOMAIN.conf
       fi
       systemctl reload php7.1-fpm
     fi
@@ -97,18 +97,18 @@ configure_fpm_pool(){
       if [ $USER_DOMAIN_TYP = "0" ]; then
         cp phpfpmpool.template /etc/php/7.0/fpm/pool.d/$USER_MAINDOMAIN.conf
 
-		sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/php/7.0/fpm/pool.d/$USER_MAINDOMAIN.conf
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/php/7.0/fpm/pool.d/$USER_MAINDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/php/7.0/fpm/pool.d/$USER_MAINDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_MAINDOMAIN_ROOT_LOCATION'|g' /etc/php/7.0/fpm/pool.d/$USER_MAINDOMAIN.conf
-		sed -i s/PHP-SOCKET/php70-fpm-$USER_DOMAIN_HYPHEN/g /etc/php/7.0/fpm/pool.d/$USER_MAINDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php70-fpm-$USER_DOMAIN_HYPHEN/g /etc/php/7.0/fpm/pool.d/$USER_MAINDOMAIN.conf
       fi
       if [ $USER_DOMAIN_TYP = "1" ]; then
         cp phpfpmpool.template /etc/php/7.0/fpm/pool.d/$USER_SUBDOMAIN.conf
-		
-		sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.0/fpm/pool.d/$USER_SUBDOMAIN.conf
+
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.0/fpm/pool.d/$USER_SUBDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/php/7.0/fpm/pool.d/$USER_SUBDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_SUBDOMAIN_ROOT_LOCATION'|g' /etc/php/7.0/fpm/pool.d/$USER_SUBDOMAIN.conf
-		sed -i s/PHP-SOCKET/php70-fpm-$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.0/fpm/pool.d/$USER_SUBDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php70-fpm-$USER_SUBDOMAIN_HYPHEN/g /etc/php/7.0/fpm/pool.d/$USER_SUBDOMAIN.conf
       fi
       systemctl reload php7.0-fpm
     fi
@@ -118,18 +118,18 @@ configure_fpm_pool(){
       if [ $USER_DOMAIN_TYP = "0" ]; then
         cp templates/phpfpmpool.template /etc/opt/remi/php72/php-fpm.d/$USER_MAINDOMAIN.conf
 
-		sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php72/php-fpm.d/$USER_MAINDOMAIN.conf
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php72/php-fpm.d/$USER_MAINDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/opt/remi/php72/php-fpm.d/$USER_MAINDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_MAINDOMAIN_ROOT_LOCATION'|g' /etc/opt/remi/php72/php-fpm.d/$USER_MAINDOMAIN.conf
-		sed -i s/PHP-SOCKET/php72-fpm-$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php72/php-fpm.d/$USER_MAINDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php72-fpm-$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php72/php-fpm.d/$USER_MAINDOMAIN.conf
       fi
       if [ $USER_DOMAIN_TYP = "1" ]; then
         cp templates/phpfpmpool.template /etc/opt/remi/php72/php-fpm.d/$USER_SUBDOMAIN.conf
-		
-		sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/opt/remi/php72/php-fpm.d/$USER_SUBDOMAIN.conf
+
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/opt/remi/php72/php-fpm.d/$USER_SUBDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/opt/remi/php72/php-fpm.d/$USER_SUBDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_SUBDOMAIN_ROOT_LOCATION'|g' /etc/opt/remi/php72/php-fpm.d/$USER_SUBDOMAIN.conf
-		sed -i s/PHP-SOCKET/php72-fpm-$USER_SUBDOMAIN_HYPHEN/g /etc/opt/remi/php72/php-fpm.d/$USER_SUBDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php72-fpm-$USER_SUBDOMAIN_HYPHEN/g /etc/opt/remi/php72/php-fpm.d/$USER_SUBDOMAIN.conf
       fi
       systemctl reload php72-php-fpm
     fi
@@ -137,19 +137,19 @@ configure_fpm_pool(){
     if [ $USER_PHP_VERSION = "php71" ]; then
       if [ $USER_DOMAIN_TYP = "0" ]; then
         cp templates/phpfpmpool.template /etc/opt/remi/php71/php-fpm.d/$USER_MAINDOMAIN.conf
-		
-		sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php71/php-fpm.d/$USER_MAINDOMAIN.conf
+
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php71/php-fpm.d/$USER_MAINDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/opt/remi/php71/php-fpm.d/$USER_MAINDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_MAINDOMAIN_ROOT_LOCATION'|g' /etc/opt/remi/php71/php-fpm.d/$USER_MAINDOMAIN.conf
-		sed -i s/PHP-SOCKET/php71-fpm-$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php71/php-fpm.d/$USER_MAINDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php71-fpm-$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php71/php-fpm.d/$USER_MAINDOMAIN.conf
       fi
       if [ $USER_DOMAIN_TYP = "1" ]; then
         cp templates/phpfpmpool.template /etc/opt/remi/php71/php-fpm.d/$USER_SUBDOMAIN.conf
 
-		sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/opt/remi/php71/php-fpm.d/$USER_SUBDOMAIN.conf
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/opt/remi/php71/php-fpm.d/$USER_SUBDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/opt/remi/php71/php-fpm.d/$USER_SUBDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_SUBDOMAIN_ROOT_LOCATION'|g' /etc/opt/remi/php71/php-fpm.d/$USER_SUBDOMAIN.conf
-		sed -i s/PHP-SOCKET/php71-fpm-$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php71/php-fpm.d/$USER_MAINDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php71-fpm-$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php71/php-fpm.d/$USER_MAINDOMAIN.conf
       fi
       systemctl reload php71-php-fpm
     fi
@@ -157,18 +157,18 @@ configure_fpm_pool(){
       if [ $USER_DOMAIN_TYP = "0" ]; then
         cp templates/phpfpmpool.template /etc/opt/remi/php70/php-fpm.d/$USER_MAINDOMAIN.conf
 
-		sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php70/php-fpm.d/$USER_MAINDOMAIN.conf
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php70/php-fpm.d/$USER_MAINDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/opt/remi/php70/php-fpm.d/$USER_MAINDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_MAINDOMAIN_ROOT_LOCATION'|g' /etc/opt/remi/php70/php-fpm.d/$USER_MAINDOMAIN.conf
-		sed -i s/PHP-SOCKET/php70-fpm-$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php70/php-fpm.d/$USER_MAINDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php70-fpm-$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php70/php-fpm.d/$USER_MAINDOMAIN.conf
       fi
       if [ $USER_DOMAIN_TYP = "1" ]; then
         cp templates/phpfpmpool.template /etc/opt/remi/php70/php-fpm.d/$USER_SUBDOMAIN.conf
 
-		sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/opt/remi/php70/php-fpm.d/$USER_SUBDOMAIN.conf
+		    sed -i s/DOMAINNAME_HYPHEN/$USER_SUBDOMAIN_HYPHEN/g /etc/opt/remi/php70/php-fpm.d/$USER_SUBDOMAIN.conf
         sed -i s/HOST_LOCATION_USER/$HOST_LOCATION_USER/g /etc/opt/remi/php70/php-fpm.d/$USER_SUBDOMAIN.conf
         sed -i 's|'HOST_DOMAIN_FULL'|'$HOST_SUBDOMAIN_ROOT_LOCATION'|g' /etc/opt/remi/php70/php-fpm.d/$USER_SUBDOMAIN.conf
-		sed -i s/PHP-SOCKET/php70-fpm-$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php70/php-fpm.d/$USER_MAINDOMAIN.conf
+		    sed -i s/PHP-SOCKET/php70-fpm-$USER_DOMAIN_HYPHEN/g /etc/opt/remi/php70/php-fpm.d/$USER_MAINDOMAIN.conf
       fi
       systemctl reload php70-php-fpm
     fi
@@ -190,8 +190,8 @@ configure_nginx_vhost(){
     cp templates/nginx_default.template /etc/nginx/conf.d/$USER_MAINDOMAIN.conf
     sed -i s/NGXSOCKET/$NGXSOCKET/g /etc/nginx/conf.d/$USER_MAINDOMAIN.conf
     sed -i s/DOMAIN_FULLNAME/$USER_MAINDOMAIN/g /etc/nginx/conf.d/$USER_MAINDOMAIN.conf
-	sed -i s/SSL_DOMAIN_FULLNAME/$USER_MAINDOMAIN/g /etc/nginx/conf.d/$USER_SUBDOMAIN.conf
-	sed -i 's|'DOMAIN_HTTPD_LOCATION'|'$HOST_MAINDOMAIN_HTTPD_LOCATION/g /etc/nginx/conf.d/$USER_SUBDOMAIN.conf
+	  sed -i s/SSL_DOMAIN_FULLNAME/$USER_MAINDOMAIN/g /etc/nginx/conf.d/$USER_SUBDOMAIN.conf
+	  sed -i 's|'DOMAIN_HTTPD_LOCATION'|'$HOST_MAINDOMAIN_HTTPD_LOCATION/g /etc/nginx/conf.d/$USER_SUBDOMAIN.conf
   fi
   if [ $USER_DOMAIN_TYP = "1" ]; then
     if [ $USER_PHP_VERSION = "php72" ]; then
@@ -206,28 +206,13 @@ configure_nginx_vhost(){
     cp templates/nginx_default.template /etc/nginx/conf.d/$USER_SUBDOMAIN.conf
     sed -i s/NGXSOCKET/$NGXSOCKET/g /etc/nginx/conf.d/$USER_SUBDOMAIN.conf
     sed -i s/DOMAIN_FULLNAME/$USER_SUBDOMAIN/g /etc/nginx/conf.d/$USER_SUBDOMAIN.conf
-	sed -i s/SSL_DOMAIN_FULLNAME/$USER_MAINDOMAIN/g /etc/nginx/conf.d/$USER_SUBDOMAIN.conf
-	sed -i 's|'DOMAIN_HTTPD_LOCATION'|'$HOST_SUBDOMAIN_HTTPD_LOCATION/g /etc/nginx/conf.d/$USER_SUBDOMAIN.conf
+	  sed -i s/SSL_DOMAIN_FULLNAME/$USER_MAINDOMAIN/g /etc/nginx/conf.d/$USER_SUBDOMAIN.conf
+	  sed -i 's|'DOMAIN_HTTPD_LOCATION'|'$HOST_SUBDOMAIN_HTTPD_LOCATION/g /etc/nginx/conf.d/$USER_SUBDOMAIN.conf
   fi
-    systemctl reload nginx
 	return 0
 }
 
-configure_letsencrypt_domain() {
-  # Need to be rewritten
-  systemctl -q stop nginx
-  if [ $USER_DOMAIN_TYP = "0" ]; then
-    certbot certonly --standalone --rsa-key-size 4096 -d $USER_MAINDOMAIN -d www.$USER_MAINDOMAIN
-    return 0
-  fi
-  if [ $USER_DOMAIN_TYP = "1" ]; then
-    certbot certonly --standalone --rsa-key-size 4096 -d $USER_MAINDOMAIN -d www.$USER_MAINDOMAIN -d $USER_SUBDOMAIN
-    return 0
-  fi
-  systemctl -q start nginx
-}
-
-create_logrorate_nginx() {
+configure_logrorate_nginx() {
   # Logrotation for nginx
   if [ $USER_DOMAIN_TYP = "0" ]; then
     cat >> /etc/logrotate.d/nginx <<EOL
@@ -263,21 +248,32 @@ EOL
     }
 EOL
   fi
+  return 0
 }
 
+configure_letsencrypt_nginx() {
+  if [ $USER_DOMAIN_TYP = "0" ]; then
+    certbot certonly --post-hook "systemctl nginx reload" --webroot -w /var/www/acme-challenges --rsa-key-size 4096 -d $USER_MAINDOMAIN -d www.$USER_MAINDOMAIN
+    return 0
+  fi
+  if [ $USER_DOMAIN_TYP = "1" ]; then
+    certbot certonly --post-hook "systemctl nginx reload" --webroot -w /var/www/acme-challenges --rsa-key-size 4096 -d $USER_MAINDOMAIN -d www.$USER_MAINDOMAIN -d $USER_SUBDOMAIN
+    return 0
+  fi
+}
 
 configure_database(){
 	echo <<EOFMW "
 #################################################################
 #
-# $0 is about to create the mysql database called '$HOST_DB_DATABASE',
-# and also will setup a mysql database user '$HOST_DB_USER'.
+# We will now create the mysql database called '$HOST_DB_DATABASE',
+# and also setup a mysql database user '$HOST_DB_USER'.
 #
 #
 # Warning: if the database exists it will be dropped, if the user
 # exists the password will be reset. (Ctrl-c to abort)
 #
-# Please provide the mysql root password if required
+# Please provide your mysql root password if required
 #################################################################
 "
 EOFMW
@@ -290,9 +286,7 @@ EOSQL
 }
 
 echo <<EOF "
-$(basename $0) will attempt to add the config files for your vhost,
-it will generate random passwords and the relevant ones will be informed.
-Also an Let's Encrypt certifcate for your vhost will be requested.
+$(basename $0) will attempt to add a vhost to your system now.
 This script is provided as it is, no warraties implied. (Ctrl-c to abort)
 "
 EOF
@@ -348,14 +342,6 @@ then
       *) echo invalid option;;
     esac
   done
-  read -p "Do you want to secure your site with SSL? [y/n]" -n 1 -r
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]
-  then
-    USER_SSL_SITE=1
-  else
-    USER_SSL_SITE=0
-  fi
 
   read -p "Do you want to add a database? [y/n]" -n 1 -r
   echo
@@ -368,9 +354,9 @@ then
   fi
 
   # Do we got everything we need?
-  
+
   HOST_LOCATION_USER=(${USER_MAINDOMAIN//./ })
-  
+
   HOST_DB_USER=$HOST_LOCATION_USER'_usr'
   if [ $USER_DOMAIN_TYP = "0" ]; then
   HOST_DB_DATABASE=$USER_MAINDOMAIN
@@ -378,23 +364,26 @@ then
   if [ $USER_DOMAIN_TYP = "1" ]; then
   HOST_DB_DATABASE=$USER_SUBDOMAIN
   fi
-  
+
   HOST_DB_PASS=$(</dev/urandom tr -dc A-Za-z0-9 | head -c10)
-  
+
   USER_DOMAIN_HYPHEN=${USER_MAINDOMAIN/./-}
   USER_SUBDOMAIN_HYPHEN=${USER_SUBDOMAIN//./-}
-  
+
   HOST_MAINDOMAIN_ROOT_LOCATION="/var/www/$USER_MAINDOMAIN"
   HOST_SUBDOMAIN_ROOT_LOCATION="/var/www/$USER_MAINDOMAIN/$USER_SUBDOMAIN"
-  
+
   HOST_MAINDOMAIN_HTTPD_LOCATION="/var/www/$USER_MAINDOMAIN/htdocs"
   HOST_SUBDOMAIN_HTTPD_LOCATION="/var/www/$USER_MAINDOMAIN/$USER_SUBDOMAIN/htdocs"
-  
+
   create_skeleton_dirs
   configure_fpm_pool
-  #configure_nginx_vhost
-  # configure_letsencrypt_domain
-  #configure_database
+  configure_nginx_vhost
+  configure_logrorate_nginx
+  configure_letsencrypt_nginx
+  if [ $USER_DB_SITE = "1" ]; then
+  configure_database
+  fi
 
   [ $? -ne "0" ] && exit 1
 fi
@@ -416,27 +405,20 @@ echo "Subdomain with hyphen: $USER_SUBDOMAIN_HYPHEN"
 
 # Debugging
 
-# echo <<EOF "
-# #################################################################
-# #
-# # SLEMP used the following enviroment:
-# #
-# # Domain: $HOST_DOMAIN_FULL
-# # Absolute path: $HOST_HTTPD_LOCATION
-# # MySQL username: $HOST_DB_USER
-# # MySQL password: $HOST_DB_PASS
-# # MyMySQL database: $HOST_DB_DATABASE
-# # Location owner: $HOST_LOCATION_USER
-# #
-# # Finish the wordpress setup by going to https://$HOST_DOMAIN and complete the famous five
-# # minute WordPress installation process.
-# #
-# # Note: In case the $HOST_DOMAIN is matching the server hostname (overlaping default site config),
-# # the site may not work, you may need to disable the default site in debian based systems or check
-# # nginx configuration
-# #################################################################
-# "
-# EOF
-
-
+echo <<EOF "
+#################################################################
+#
+# Your domain is ready to use:
+#
+# Domain: $USER_MAINDOMAIN
+# Absolute path: $HOST_MAINDOMAIN_ROOT_LOCATION
+# MySQL username: $HOST_DB_USER
+# MySQL password: $HOST_DB_PASS
+# MyMySQL database: $HOST_DB_DATABASE
+# Location owner: $HOST_LOCATION_USER
+#
+#
+#################################################################
+"
+EOF
 exit 0
